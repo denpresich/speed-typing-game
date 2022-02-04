6be6c1323e1c45ec27f1a12e2b5b63170237790e
Denys Presich <denys.presich@gmail.com>
2022-02-05 01:11:04 +0200

    Success/Fail letter animation

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,6 +1,7 @@
 import "./game.css";
 
 import React from "react";
+import randomWords from "random-words";
 
 import Word from "../word";
 
@@ -9,13 +10,20 @@ import reducer, { INITIAL_STATE, ACTION_TYPES } from "./reducer";
 export default function Game() {
   const [state, dispatch] = React.useReducer(reducer, INITIAL_STATE);
 
+  React.useEffect(() => {
+    const words = randomWords(100);
+    dispatch({ type: ACTION_TYPES.setWords, payload: { words } });
+  }, []);
+
   const handleKeyDown = (event) => {
     const correct = event.key === state.word[state.letterSuccessCount];
 
     if (correct) {
-      state.word.length - 1 === state.letterSuccessCount
-        ? dispatch({ type: ACTION_TYPES.setNextWord })
-        : dispatch({ type: ACTION_TYPES.incrementLetterSuccess });
+      dispatch({ type: ACTION_TYPES.incrementLetterSuccess });
+
+      if (state.word.length - 1 === state.letterSuccessCount) {
+        setTimeout(() => dispatch({ type: ACTION_TYPES.setNextWord }), 300);
+      }
     } else {
       dispatch({ type: ACTION_TYPES.setLetterFail });
     }
@@ -30,7 +38,7 @@ export default function Game() {
   return (
     <div className="game">
       <Word
-        word={state.word}
+        word={state.word || ""}
         successCount={state.letterSuccessCount}
         lastFail={state.letterLastFail}
       />
--- src/components/game/reducer.js
+++ src/components/game/reducer.js
@@ -1,21 +1,28 @@
-import WORDS from "../../constants/words";
-
 export const INITIAL_STATE = {
-  word: WORDS[0],
+  words: [],
+  word: null,
   wordIndex: 0,
   letterSuccessCount: 0,
   letterLastFail: false,
 };
 
 export const ACTION_TYPES = {
+  setWords: "SET_WORDS",
   setNextWord: "SET_NEXT_WORD",
   incrementLetterSuccess: "INCREMENT_LETTER_SUCCESS",
   setLetterFail: "SET_LETTER_FAIL",
 };
 
-const setNextWord = (state) => ({
+const setWordsReducer = (state, { payload }) => ({
   ...state,
-  word: WORDS[state.wordIndex + 1],
+  words: payload.words,
+  word: payload.words[0],
+  wordIndex: 0,
+});
+
+const setNextWordReducer = (state) => ({
+  ...state,
+  word: state.words[state.wordIndex + 1],
   wordIndex: state.wordIndex + 1,
   letterSuccessCount: 0,
   letterLastFail: false,
@@ -24,6 +31,7 @@ const setNextWord = (state) => ({
 const incrementLetterSuccessReducer = (state) => ({
   ...state,
   letterSuccessCount: state.letterSuccessCount + 1,
+  letterLastFail: false,
 });
 
 const setLetterFailReducer = (state) => ({
@@ -33,8 +41,10 @@ const setLetterFailReducer = (state) => ({
 
 export default function reducer(state, action) {
   switch (action.type) {
+    case ACTION_TYPES.setWords:
+      return setWordsReducer(state, action);
     case ACTION_TYPES.setNextWord:
-      return setNextWord(state);
+      return setNextWordReducer(state);
     case ACTION_TYPES.incrementLetterSuccess:
       return incrementLetterSuccessReducer(state);
     case ACTION_TYPES.setLetterFail:
