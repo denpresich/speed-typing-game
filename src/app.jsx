db4d6c27378f130b021d076bdad27abf293dd7e7
Denys Presich <denys.presich@gmail.com>
2022-02-07 00:33:37 +0200

    ✨ Add google analytics

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import ReactGA from "react-ga";
 
 import Game from "./components/game";
+
+ReactGA.initialize("UA-219589386-1");
+ReactGA.pageview(window.location.href);
 
 export default function App() {
   return <Game />;
