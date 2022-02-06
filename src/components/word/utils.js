d7ab2b8aa22721638848d216dafa9837cd6c349f
Denys Presich <denys.presich@gmail.com>
2022-02-06 13:15:49 +0200

    Add ESLint

--- src/components/word/utils.js
+++ src/components/word/utils.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export const getLetterVariant = (currentIndex, successCount, lastFail) => {
   if (currentIndex <= successCount - 1) {
     return "success";
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import "./styles/global.css";
 
 import React from "react";
